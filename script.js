57d9a69c4d15e6a851f67928540453660e96c6cb
yasha1w <naoapenas1@gmail.com>
2024-12-12 16:00:08 -0500

    subir arquivo

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = parseInt (prompt ("digite o primeiro valor"))
+let num2 = parseInt (prompt ("digite o segundo numero"))
+let result = num1 + num2
+
+if(num1 + num2){
+    alert("a soma dos dois numeros é "+ result)
+} else {
+    alert("valor invalido")
+}
